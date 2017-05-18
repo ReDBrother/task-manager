b2325f74a4da91e533777671b3e0a355d16e3180
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-05-18 13:03:01 +0300

    add postcss

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -20,7 +20,7 @@ export default () => ({
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
