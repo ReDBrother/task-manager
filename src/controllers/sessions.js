b514e79cdd10e8558485868272270706509dae5b
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-05-22 01:18:50 +0300

    fixes

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -1,5 +1,5 @@
 import buildFormObj from '../lib/formObjectBuilder';
-import { encrypt } from '../lib/secure';
+import encrypt from '../lib/secure';
 
 export default (router, { User }) => {
   router
